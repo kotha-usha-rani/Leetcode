d0e8add1a0cccd9c6abb95b759304d9a27ec263e
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-01-19 18:06:27 +0530

    Time: 2 ms (30.24%), Space: 54.1 MB (5.06%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -1,10 +1,21 @@
 class Solution {
+    public void reverse(char[] s, int i, int j){
+        if(i>=j)
+            return;
+        char ch = s[i];
+        s[i] = s[j];
+        s[j] = ch;
+        reverse(s, i+1, j-1);
+    }
     public void reverseString(char[] s) {
         //Method 1 using two pointer approach
-        for(int i=0 ; i<s.length/2 ; i++){
-            char ch = s[i];
-            s[i] = s[s.length-i-1];
-            s[s.length-i-1] = ch;
-        }
+        // for(int i=0 ; i<s.length/2 ; i++){
+        //     char ch = s[i];
+        //     s[i] = s[s.length-i-1];
+        //     s[s.length-i-1] = ch;
+        // }
+        
+        //Method 2 using recursion
+        reverse(s, 0, s.length-1);
     }
 }
