8352c00a3cb8c2842945045367ded7de50237dae
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-01-19 18:27:27 +0530

    Added solution - LeetHub

--- Palindrome String - GFG/palindrome-string.java
+++ Palindrome String - GFG/palindrome-string.java
@@ -22,15 +22,27 @@ class GFG {
 //User function Template for Java
 
 class Solution {
+    int check(String S, int i, int j){
+        if(i>=j)
+            return 1 ;
+        if(S.charAt(i) != S.charAt(j))
+            return 0;
+        return check(S, i+1, j-1);
+    }
     int isPalindrome(String S) {
         // code here
-    int len = S.length();
-    if(len == 1)
-        return 1;
-    for(int i=0 ; i<len ; i++){
-        if(S.charAt(i) != S.charAt(len-i-1))
-            return 0;
-    }
-    return 1;
+    //Method 1 two pointer approach
+    // int len = S.length();
+    // if(len == 1)
+    //     return 1;
+    // for(int i=0 ; i<len ; i++){
+    //     if(S.charAt(i) != S.charAt(len-i-1))
+    //         return 0;
+    // }
+    // return 1;
+    // }
+    
+    //Method 2 Recursion
+    return check(S, 0, S.length()-1);
     }
 };
