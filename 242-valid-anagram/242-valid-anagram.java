a55b53d640f0c997f2b9b4ea9083bc6dc6aec223
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-01-20 15:30:23 +0530

    Time: 7 ms (46.82%), Space: 40.4 MB (41.15%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -16,24 +16,37 @@ class Solution {
         // }
         // return true;
         
-        //store the count of characters of one string in a hashmap, check if all the 
+        //Method 2 store the count of characters of one string in a hashmap, check if all the 
         //characters of another string are present
-        HashMap<Character, Integer> map = new HashMap<>();
-        for(int i=0; i<s.length() ; i++){
-            char ch = s.charAt(i);
-            if(map.containsKey(ch))
-                map.put(ch, map.get(ch)+1);
-            else
-                map.put(ch, 1);
+        // HashMap<Character, Integer> map = new HashMap<>();
+        // for(int i=0; i<s.length() ; i++){
+        //     char ch = s.charAt(i);
+        //     if(map.containsKey(ch))
+        //         map.put(ch, map.get(ch)+1);
+        //     else
+        //         map.put(ch, 1);
+        // }
+        // for(int i=0 ; i<t.length() ; i++){
+        //     char ch = t.charAt(i);
+        //     if(map.containsKey(ch) && map.get(ch) > 0)
+        //         map.put(ch, map.get(ch)-1);
+        //     else
+        //         return false;
+        // }
+        // return true;
+        
+        //Method 3 store the character count in an array of size 26
+        int[] arr = new int[26];
+        for(int i=0 ; i<s.length() ; i++){
+            arr[s.charAt(i)-'a']++;
         }
         for(int i=0 ; i<t.length() ; i++){
-            char ch = t.charAt(i);
-            if(map.containsKey(ch) && map.get(ch) > 0)
-                map.put(ch, map.get(ch)-1);
-            else
+            arr[t.charAt(i)-'a']--;
+            if(arr[t.charAt(i)-'a'] < 0)
                 return false;
         }
         return true;
+        
         
     }
 }
