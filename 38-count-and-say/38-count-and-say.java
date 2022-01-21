2607b62c98cec5a775fa3fe8303d33a6554f41e2
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-01-21 10:19:03 +0530

    Time: 3 ms (77.25%), Space: 38.6 MB (55.05%) - LeetHub

--- 38-count-and-say/38-count-and-say.java
+++ 38-count-and-say/38-count-and-say.java
@@ -1,30 +1,58 @@
 class Solution {
+    
+    public ArrayList<Integer> cAS(int temp, int n, ArrayList<Integer> current){
+        if(temp > n)
+            return current;
+        ArrayList<Integer> smallAns = new ArrayList<>();
+        for(int i=0 ; i<current.size() ;){
+            int ch = current.get(i);
+            int count = 0;
+            while(i<current.size() && current.get(i) == ch){
+                count++;
+                i++;
+            }
+            smallAns.add(count);
+            smallAns.add(ch);
+        }
+        return cAS(temp+1, n, smallAns);
+        
+    }
+    
     public String countAndSay(int n) {
-        //Method 1 Interation
         if(n==1)
             return "1";
-       ArrayList<Integer> ans = new ArrayList<>();
-       ans.add(1);
-       for(int i=2 ; i<=n ; i++){
-           ArrayList<Integer> temp = new ArrayList<>();
-           for(int j=0 ; j<ans.size() ;){
-               int count = 0 ;
-               int current = ans.get(j);
-               while(j<ans.size() && ans.get(j) == current){
-                   count++;
-                   j++;
-               }
-               temp.add(count);
-               temp.add(current);
-           }
-           ans = temp ;
-           
-       } 
-        StringBuilder sb = new StringBuilder();
-        for(int i=0 ; i<ans.size() ; i++){
-            sb.append(ans.get(i));
-        }
-        return sb.toString(); 
         
+        //Method 1 Interation store the current answer, count and create a new answer, update           //the original/current answer
+       // ArrayList<Integer> ans = new ArrayList<>();
+       // ans.add(1);
+       // for(int i=2 ; i<=n ; i++){
+       //     ArrayList<Integer> temp = new ArrayList<>();
+       //     for(int j=0 ; j<ans.size() ;){
+       //         int count = 0 ;
+       //         int current = ans.get(j);
+       //         while(j<ans.size() && ans.get(j) == current){
+       //             count++;
+       //             j++;
+       //         }
+       //         temp.add(count);
+       //         temp.add(current);
+       //     }
+       //     ans = temp ;
+       // } 
+       //  StringBuilder sb = new StringBuilder();
+       //  for(int i=0 ; i<ans.size() ; i++){
+       //      sb.append(ans.get(i));
+       //  }
+       //  return sb.toString(); 
+        
+        //Recursion
+        ArrayList<Integer> current = new ArrayList<>();
+        current.add(1);
+        current = cAS(2, n, current);
+        StringBuilder ans = new StringBuilder();
+        for(int i=0 ; i<current.size() ; i++){
+            ans.append(current.get(i));
+        }
+        return ans.toString();
     }
 }
