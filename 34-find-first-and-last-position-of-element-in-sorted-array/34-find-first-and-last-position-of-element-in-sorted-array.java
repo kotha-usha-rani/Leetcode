5d41abcc1affdd97b4d5207924c6227943ea5278
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-01-24 14:46:21 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (49.43%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -39,11 +39,34 @@ class Solution {
         if(nums.length==0)
             return new int[]{-1, -1};
         int[] ans = new int[2];
-        ans[0] = binarySearch(nums, 0, nums.length-1, target, true);
-        if(ans[0] != -1)
-            ans[1] = binarySearch(nums, ans[0], nums.length-1, target, false);
-        else
-            ans[1] = -1;
+        ans[0] = -1;
+        ans[1] = -1;
+        //Method 1 Linear Search
+        for(int i=0 ; i<nums.length ; i++){
+            if(nums[i] == target){
+                ans[0] = i;
+                if(i+1 < nums.length && nums[i+1] != target){
+                    ans[1] = i;
+                    return ans;
+                }
+                break;
+            }
+        }
+        if(ans[0] == -1)
+            return ans;
+        for(int i=nums.length-1 ; i>=0 ; i--){
+            if(nums[i] ==  target){
+                ans[1] = i;
+                return ans;
+            }
+        }
         return ans;
+        //Method 2 binarySearch
+        // ans[0] = binarySearch(nums, 0, nums.length-1, target, true);
+        // if(ans[0] != -1)
+        //     ans[1] = binarySearch(nums, ans[0], nums.length-1, target, false);
+        // else
+        //     ans[1] = -1;
+        // return ans;
     }
 }
