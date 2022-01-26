dc01202f2c3d56966a46d023dac3c92659ce55b5
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-01-26 14:06:36 +0530

    Added solution - LeetHub

--- Find Missing And Repeating - GFG/find-missing-and-repeating.java
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.java
@@ -30,30 +30,60 @@ class GFG {
 
 class Solve {
     int[] findTwoElement(int arr[], int n) {
-        //Method 1 sort the array
-        Arrays.sort(arr);
-        int step = arr[0] ;
-        int prev = 0;
+        //Method 1 sort the array O(nlogn)+n
+        // Arrays.sort(arr);
+        // int step = arr[0] ;
+        // int prev = 0;
+        // int[] ans = new int[2];
+        // ans[1] = arr[0]-1;
+        // int i=0;
+        // for(; i<n ; i++){
+        //     if(arr[i] == prev){
+        //         ans[0] = arr[i];
+        //         step-- ;65
+        //         i++;
+        //         if(i >= n)
+        //             break;
+        //     }
+        //     if(arr[i] != i+step){
+        //         ans[1] = i+step ; 
+        //         step++;
+        //         i--;
+        //     }
+        //     prev = arr[i];
+        // }
+        // if(ans[1] == 0)
+        //     ans[1] = i;
+        // return ans;,
+        
+        //Method 2 consider elements as indices, mark negative for element at ele-1
         int[] ans = new int[2];
-        ans[1] = arr[0]-1;
-        int i=0;
-        for(; i<n ; i++){
-            if(arr[i] == prev){
-                ans[0] = arr[i];
-                step-- ;
+        ans[1] = 0 ;
+        for(int i=0 ; i<arr.length ; i++){
+            if(arr[Math.abs(arr[i])-1] < 0){
+                ans[0] = Math.abs(arr[i]);
                 i++;
-                if(i >= n)
+                if(i>=arr.length)
                     break;
             }
-            if(arr[i] != i+step){
-                ans[1] = i+step ; 
-                step++;
-                i--;
+            arr[Math.abs(arr[i])-1] = -arr[Math.abs(arr[i])-1];
+        }
+        int i=0;
+        for(; i<arr.length ; i++){
+            if(arr[i] > 0){
+                ans[1] = i+1 ;
+                return ans;
             }
-            prev = arr[i];
         }
         if(ans[1] == 0)
             ans[1] = i;
         return ans;
     }
 }
+
+
+
+
+
+
+
