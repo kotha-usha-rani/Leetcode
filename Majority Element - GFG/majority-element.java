d2ca0f792ca9711c684cd5d96197ef214aa3b9a1
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-01-26 20:21:16 +0530

    Added solution - LeetHub

--- Majority Element - GFG/majority-element.java
+++ Majority Element - GFG/majority-element.java
@@ -32,28 +32,45 @@ class Solution
 {
     static int majorityElement(int nums[], int size)
     {
-        // your code here
-        Arrays.sort(nums);
-        int count = 1;
-        int max = 0;
-        int ele = 0 ;
-        int prev = nums[0];
-        for(int i=1 ; i<nums.length ; i++){
-            if(nums[i] == prev)
-                count++;
-            else{
-                if(count > max){
-                    max = count;
-                    ele = prev ;
-                }
-                prev = nums[i];
-                count = 1 ;
+        //Method 1 sort the array, find the max count of each element, return the ele with max count
+        // Arrays.sort(nums);
+        // int count = 1;
+        // int max = 0;
+        // int ele = 0 ;
+        // int prev = nums[0];
+        // for(int i=1 ; i<nums.length ; i++){
+        //     if(nums[i] == prev)
+        //         count++;
+        //     else{
+        //         if(count > max){
+        //             max = count;
+        //             ele = prev ;
+        //         }
+        //         prev = nums[i];
+        //         count = 1 ;
+        //     }
+        // }
+        // if(count > max && count > size/2)
+        //     return prev;
+        // if(max > size/2)
+        //     return ele ;
+        // return -1;   
+        
+        //HashMap to store the count of elements--> time limit exceeded
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0 ; i<nums.length ; i++){
+            if(map.containsKey(nums[i])){
+              map.put(nums[i], map.get(nums[i])+1);
+              if(map.get(nums[i]) > size/2)
+                return nums[i];
             }
+            else
+              map.put(nums[i], 1);
         }
-        if(count > max && count > size/2)
-            return prev;
-        if(max > size/2)
-            return ele ;
-        return -1;        
+        for(Integer ele : map.keySet()){
+            if(map.get(ele) > size/2)
+                return ele;
+        }
+        return -1;
     }
 }
