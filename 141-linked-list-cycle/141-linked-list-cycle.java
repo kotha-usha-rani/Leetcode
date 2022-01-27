39635bd6c8e9ec933c6abfcf16f445b07e6aa2aa
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-01-27 15:07:35 +0530

    Time: 4 ms (19.78%), Space: 43.1 MB (17.03%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -11,16 +11,34 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
+        
+        //Method 1 Using fast and slow pointers
+       //  if(head == null)
+       //      return false;
+       //  ListNode slow = head;
+       //  ListNode fast = head;
+       //  while(fast != null && fast.next != null){
+       //      fast = fast.next.next;
+       //      slow = slow.next;
+       //      if(fast == slow)
+       //          return true;
+       //  }
+       // return false;
+        
+        //Using hashmaps
         if(head == null)
             return false;
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast != null && fast.next != null){
-            fast = fast.next.next;
-            slow = slow.next;
-            if(fast == slow)
+        HashMap<ListNode, Boolean> map = new HashMap<>();
+        while(head.next != null){
+            if(map.containsKey(head))
                 return true;
+            else
+                map.put(head, true);
+            head = head.next;
         }
-       return false;
+        return false;
     }
 }
+
+
+
