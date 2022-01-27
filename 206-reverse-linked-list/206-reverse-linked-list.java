60f1b80c878acf18a82c899e02e958546d0cd547
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-01-27 07:29:06 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (7.86%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -9,23 +9,32 @@
  * }
  */
 class Solution {
+    public ListNode reverse(ListNode head){
+        if(head == null || head.next == null)
+            return head ;
+        ListNode newHead = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
     public ListNode reverseList(ListNode head) {
         //Method 1 change the links TC=O(n), SC=O(1)
-        if(head == null || head.next == null)
-            return head;
-        ListNode prev = null;
-        ListNode nextNode = null;
-        ListNode current = head;
-        while(current != null){
-            nextNode = current.next;
-            current.next = prev ;
-            prev = current;
-            current = nextNode;
-        }
-        return prev;
+        // if(head == null || head.next == null)
+        //     return head;
+        // ListNode prev = null;
+        // ListNode nextNode = null;
+        // ListNode current = head;
+        // while(current != null){
+        //     nextNode = current.next;
+        //     current.next = prev ;
+        //     prev = current;
+        //     current = nextNode;
+        // }
+        // return prev;
         
         //Method 2 recursion
-        
+        ListNode newHead = reverse(head);
+        return newHead;
         
     }
 }
