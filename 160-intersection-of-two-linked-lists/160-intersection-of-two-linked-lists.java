88dd2d0174ce9322fadc87f57b3266f7bfb53ae1
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-01-29 07:17:31 +0530

    Time: 2 ms (58.11%), Space: 55.5 MB (5.41%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -19,6 +19,14 @@ public class Solution {
         }
         return null;
     }
+    public int getCount(ListNode head){
+        int c = 0;
+         while(head != null){
+            c++;
+            head = head.next;
+        }
+        return c;
+    }
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         //Method 1 using two loops
         // ListNode temp1 = headA;
@@ -37,32 +45,46 @@ public class Solution {
         // return ans;
         
         //Method 2 using count of nodes
+        // ListNode temp1 = headA;
+        // ListNode temp2 = headB;
+        // int c1 = getCount(temp1);
+        // int c2 = getCount(temp2);
+        // if(c1 == c2)
+        //     return intersection(headA, headB);
+        // if(c1 > c2){
+        //     temp1 = headA; temp2 = headB;
+        // }
+        // else{
+        //     temp1 = headB; temp2 = headA;
+        // }
+        // int diff = Math.abs(c1-c2);
+        // while(temp1 != null && diff > 0){
+        //     diff--;
+        //     temp1 = temp1.next;
+        // }
+        // return intersection(temp1, temp2);
+        
+        //Method 3 using two pointers
         ListNode temp1 = headA;
         ListNode temp2 = headB;
-        int c1 = 0; int c2 = 0;
-        while(temp1 != null){
-            c1++;
-            temp1 = temp1.next;
+        int count = 0 ;
+        while(temp1 != null || temp2 != null){
+            if(temp1 == temp2)
+                return temp1;
+            temp1 = temp1.next ;
+            temp2 = temp2.next ;
+            
+            if(temp1 == null){
+                count++;
+                if(count == 2)
+                    break;
+                temp1 = headB;
+            }
+            if(temp2 == null){
+                temp2 = headA;
+            }
         }
-        while(temp2 != null){
-            c2++;
-            temp2 = temp2.next;
-        }
-        if(c1 == c2)
-            return intersection(headA, headB);
-        if(c1 > c2){
-            temp1 = headA; temp2 = headB;
-        }
-        else{
-            temp1 = headB; temp2 = headA;
-        }
-        int diff = Math.abs(c1-c2);
-        while(temp1 != null && diff > 0){
-            diff--;
-            temp1 = temp1.next;
-        }
-        return intersection(temp1, temp2);
-        
+        return null;
     }
 }
 
