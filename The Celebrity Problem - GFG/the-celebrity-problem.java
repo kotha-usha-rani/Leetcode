e48315eab8ef2020c36d26cb3b681fd09976fd73
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-02-05 20:32:10 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.java
+++ The Celebrity Problem - GFG/the-celebrity-problem.java
@@ -35,21 +35,21 @@ class Solution
     int celebrity(int arr[][], int n)
     {
        //Method 1 using two loops
-        for(int i=0 ; i<n ; i++){
-            int j=0;
-            if(i==0)
-                j = 1 ;
-            if(j<n && arr[j][i] == 1){
-                while(j<n){
-                    if(arr[i][j] != 0)
-                        break;
-                    j++;
-                }
-            }
-            if(j==n)
-                return i ;
-        }
-        return -1;
+        // for(int i=0 ; i<n ; i++){
+        //     int j=0;
+        //     if(i==0)
+        //         j = 1 ;
+        //     if(j<n && arr[j][i] == 1){
+        //         while(j<n){
+        //             if(arr[i][j] != 0)
+        //                 break;
+        //             j++;
+        //         }
+        //     }
+        //     if(j==n)
+        //         return i ;
+        // }
+        // return -1;
     
     
     //Method 2 using two arrays of size n
@@ -66,5 +66,34 @@ class Solution
         //         return i;
         // }
         // return -1;
+        
+    //Method 3 using a stack
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0 ; i<n ; i++)
+            stack.push(i);
+        while(stack.size() > 1){
+            int r = stack.pop();
+            int c = stack.pop();
+            if(arr[r][c] == 1)
+                stack.push(c);
+            else
+                stack.push(r);
+        }
+        if(stack.isEmpty())
+            return -1;
+        int cele = stack.pop();
+        for(int i=0 ; i<n ; i++){
+            if(cele != i)
+                if(arr[cele][i] == 1 || arr[i][cele] == 0)
+                    return -1;
+        }
+        return cele;
     }
 }
+
+
+
+
+
+
+
