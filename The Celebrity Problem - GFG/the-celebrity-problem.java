0c2f0c75eabcb796135ec92a866477ab49bc9eab
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-02-05 16:38:44 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.java
+++ The Celebrity Problem - GFG/the-celebrity-problem.java
@@ -35,41 +35,36 @@ class Solution
     int celebrity(int arr[][], int n)
     {
        //Method 1 using two loops
-    // 	for(int i=0 ; i<n ; i++){
-    // 	    for(int j=0 ; j<n ; j++){
-    // 	        if(arr[j][i] == 1 && arr[i][j] != 1){
-    // 	            int k = 0 ;
-    // 	            boolean isCelebrity = true;
-    // 	            while(k<n){
-    // 	                if(arr[i][k] == 1){
-    // 	                    isCelebrity = false;
-    // 	                    break;
-    // 	                }
-    // 	                k++;
-    // 	            }
-    // 	            if(isCelebrity)
-    // 	                return i ;
-    // 	            else
-    // 	                break;
-    // 	        }
-    // 	    }
-    // 	}
-    // 	return -1;
+        for(int i=0 ; i<n ; i++){
+            int j=0;
+            if(i==0)
+                j = 1 ;
+            if(j<n && arr[j][i] == 1){
+                while(j<n){
+                    if(arr[i][j] != 0)
+                        break;
+                    j++;
+                }
+            }
+            if(j==n)
+                return i ;
+        }
+        return -1;
     
     
     //Method 2 using two arrays of size n
-        int[] count = new int[n];
-        for(int i=0 ; i<n ; i++){
-            for(int j=0 ; j<n ; j++){
-                if(arr[i][j] == 1 && arr[j][i] != 1){
-                    count[j]++;
-                }
-            }
-        }
-        for(int i=0 ; i<n ; i++){
-            if(count[i] == n-1)
-                return i;
-        }
-        return -1;
+        // int[] count = new int[n];
+        // for(int i=0 ; i<n ; i++){
+        //     for(int j=0 ; j<n ; j++){
+        //         if(arr[i][j] == 1 && arr[j][i] != 1){
+        //             count[j]++;
+        //         }
+        //     }
+        // }
+        // for(int i=0 ; i<n ; i++){
+        //     if(count[i] == n-1)
+        //         return i;
+        // }
+        // return -1;
     }
 }
