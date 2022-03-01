b932f62a84ae16f81eb9ab278c2f4732b712fca9
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-01 20:53:58 +0530

    Added solution - LeetHub

--- k largest elements - GFG/k-largest-elements.java
+++ k largest elements - GFG/k-largest-elements.java
@@ -31,11 +31,22 @@ public class Main {
 
 class Solution {
     int[] kLargest(int[] arr, int n, int k) {
+        //sort the array, return k largest elements
+        // int[] ans = new int[k];
+        // Arrays.sort(arr);
+        // int j=0;
+        // for(int i=n-1 ; i>=n-k ; i--)
+        //     ans[j++] = arr[i];
+        // return ans;
+        
+        //using heaps
+        PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0 ; i<n ; i++)
+            heap.add(arr[i]);
         int[] ans = new int[k];
-        Arrays.sort(arr);
-        int j=0;
-        for(int i=n-1 ; i>=n-k ; i--)
-            ans[j++] = arr[i];
+        for(int i=0 ; i<k ; i++)
+            ans[i] = heap.remove();
+            
         return ans;
     }
 }
