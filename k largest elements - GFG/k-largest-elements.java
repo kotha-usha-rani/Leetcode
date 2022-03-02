3a51e927c5178729ec32443d28ed45c939de6b3b
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-02 07:55:12 +0530

    Added solution - LeetHub

--- k largest elements - GFG/k-largest-elements.java
+++ k largest elements - GFG/k-largest-elements.java
@@ -39,14 +39,38 @@ class Solution {
         //     ans[j++] = arr[i];
         // return ans;
         
-        //using heaps
-        PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
-        for(int i=0 ; i<n ; i++)
-            heap.add(arr[i]);
-        int[] ans = new int[k];
-        for(int i=0 ; i<k ; i++)
-            ans[i] = heap.remove();
+        //using maxheap 
+        // PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
+        // for(int i=0 ; i<n ; i++)
+        //     heap.add(arr[i]);
+        // int[] ans = new int[k];
+        // for(int i=0 ; i<k ; i++)
+        //     ans[i] = heap.remove();
             
+        // return ans;
+        
+        //using minheap, store k largest elements
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        int i=0;
+        for(; i<k ; i++){
+            minHeap.add(arr[i]);
+        }
+        for( i=k ; i<n ; i++){
+            if(minHeap.peek() < arr[i]){
+                minHeap.remove();
+                minHeap.add(arr[i]);
+            }
+        }
+        int[] ans = new int[k];
+        i=k;
+        while(!minHeap.isEmpty()){
+            ans[--k] = minHeap.remove();
+        }
         return ans;
     }
 }
+
+
+
+
+
