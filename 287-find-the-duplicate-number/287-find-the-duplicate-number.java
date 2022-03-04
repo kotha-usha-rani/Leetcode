fd8a4c95f8e4912612508df81ad5b2389a5f8a2f
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-04 12:15:09 +0530

    Time: 4 ms (95.03%), Space: 76.7 MB (16.18%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -9,16 +9,26 @@ class Solution {
         // return 0;
         
         //Method 2 using cyclic sort to sort the array
-        for(int i=0 ; i<arr.length ; ){
-            int index = arr[i]-1;
-            if(arr[i] != arr[index]){
-                int temp = arr[i];
-                arr[i] = arr[index];
-                arr[index] = temp;
-            }
-            else
-                i++;
+        // for(int i=0 ; i<arr.length ; ){
+        //     int index = arr[i]-1;
+        //     if(arr[i] != arr[index]){
+        //         int temp = arr[i];
+        //         arr[i] = arr[index];
+        //         arr[index] = temp;
+        //     }
+        //     else
+        //         i++;
+        // }
+        // return arr[arr.length-1];
+        
+        //Method 3: if the values in the array can be changed, make the numbers visited as neagative            using the element at current index as index
+        int index = 0;
+        for(int i=0 ; i<arr.length ; i++){
+            index = Math.abs(arr[i]);
+            if(arr[index] < 0)
+                return index;
+            arr[index] = -arr[index];
         }
-        return arr[arr.length-1];
+        return 0;
     }
 }
