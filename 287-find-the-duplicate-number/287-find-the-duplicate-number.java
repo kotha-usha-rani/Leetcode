e909498dc365b61594a1d05d499a80e51cd1d080
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-04 09:00:27 +0530

    Time: 6 ms (71.27%), Space: 59.5 MB (85.18%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,24 +1,24 @@
 class Solution {
     public int findDuplicate(int[] arr) {
         //Method 1 using sort
-        Arrays.sort(arr);
-        for(int i=1 ; i<arr.length ; i++){
-            if(arr[i] == arr[i-1])
-               return arr[i];
-        }
-        return 0;
+        // Arrays.sort(arr);
+        // for(int i=1 ; i<arr.length ; i++){
+        //     if(arr[i] == arr[i-1])
+        //        return arr[i];
+        // }
+        // return 0;
         
         //Method 2 using cyclic sort to sort the array
-        // for(int i=0 ; i<arr.length ; i++){
-        //     if(arr[i] == i+1 || arr[i] == arr[arr[i]-1])
-        //         i++;
-        //     else{
-        //         int temp = arr[i];
-        //         arr[i] = arr[arr[i]-1];
-        //         arr[temp-1] = temp;
-        //     }
-        // }
-        // return arr[arr.length-1];
-        
+        for(int i=0 ; i<arr.length ; ){
+            int index = arr[i]-1;
+            if(arr[i] != arr[index]){
+                int temp = arr[i];
+                arr[i] = arr[index];
+                arr[index] = temp;
+            }
+            else
+                i++;
+        }
+        return arr[arr.length-1];
     }
 }
