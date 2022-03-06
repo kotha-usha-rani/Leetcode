7f8a3ea7f1d4c54e1760258dfda99e122a8dc0e6
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-06 10:26:46 +0530

    Time: 17 ms (34.44%), Space: 55.5 MB (15.56%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -3,7 +3,7 @@ class Solution {
         //Method 1: sort the array, find the intervals
         Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
         int i=0, j, max, k=0;
-        ArrayList<Integer> tempAns = new ArrayList<>();
+        ArrayList<int[]> tempAns = new ArrayList<>();
         while(i<arr.length){
             j=i;
             max = arr[i][1];
@@ -11,15 +11,10 @@ class Solution {
                 max = Math.max(max, arr[j+1][1]);
                 j++;
             }
-            tempAns.add(arr[i][0]);
-            tempAns.add(max);
+            tempAns.add(new int[]{arr[i][0], max});
             i = j+1;            
         }
-        int[][] ans = new int[tempAns.size()/2][2];
-        for(i=0 ; i<tempAns.size() ; i+=2){
-            ans[k][0] = tempAns.get(i);
-            ans[k++][1] = tempAns.get(i+1);
-        }
-        return ans;
+        //int[][] ans = new int[tempAns.size()/2][2];
+        return tempAns.toArray(new int[tempAns.size()/2][2]);
     }
 }
