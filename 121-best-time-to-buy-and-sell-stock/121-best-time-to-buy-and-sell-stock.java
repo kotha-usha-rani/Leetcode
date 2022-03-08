748a7e44945e51aafecd4af727be01c22ba23d6a
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-08 12:51:15 +0530

    Time: 4 ms (31.40%), Space: 77.3 MB (68.00%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        //Method 1: find profit for each stock, return max
+        // if(prices.length == 1)
+        //     return 0;
+        // int max = 0;
+        // for(int i=0 ; i<prices.length-1 ; i++){
+        //     for(int j=i+1 ; j<prices.length ; j++){
+        //         if(prices[i] < prices[j]){
+        //             max = Math.max(max, prices[j] - prices[i]);
+        //         }
+        //     }
+        // }
+        // return max;
+        
+        //Method 2 : using two pointer method
+        if(prices.length == 1)
+            return 0;
+        int left = Integer.MAX_VALUE, right = 0, max=0, i=0;
+        for(; i<prices.length-1 ; i+=2){
+            if(prices[i] < prices[i+1]){
+                if(prices[i] < left){
+                    left = prices[i]; 
+                    if(prices[i+1] < right)
+                        right = 0;
+                }
+                if(prices[i+1] > right)
+                    right = prices[i+1];
+            }
+            else {
+                if(prices[i] > right){
+                    right = prices[i];
+                    max = Math.max(max, right-left);
+                }
+                if(prices[i+1] < left){
+                    left = prices[i+1];
+                    right = 0;
+                }
+            }
+            if(right != 0)
+                max = Math.max(max, right-left);
+        }
+        if(i < prices.length){
+            if(prices[i] > right){
+                max = Math.max(max, prices[i]-left);
+            }
+        }
+        return max ;
+    }
+}
