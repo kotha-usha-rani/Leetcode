e677b3d9c1729802b3b06401665ffd9ec764eec2
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-10 08:56:17 +0530

    Time: 1 ms (100.00%), Space: 47.7 MB (25.76%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -10,30 +10,60 @@
  */
 class Solution {
     
-   
+    public ListNode sort(ListNode l1, ListNode l2){
+        if(l1 == null)
+            return l2;
+        if(l2 == null)
+            return l1;
+        
+        ListNode head, temp, t;
+        if(l1.val <= l2.val){
+            head = l1;
+            l1 = l1.next;
+        }
+        else{
+            head = l2 ;
+            l2 = l2.next;
+        }
+        temp = head ;
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                t = l1.next;
+                temp.next = l1 ;
+                l1 = t ;
+            }
+            else{
+                t = l2.next;
+                temp.next = l2 ;
+                l2 = t ;
+            }
+            temp = temp.next;
+        }
+        if(l1 != null){
+            temp.next = l1 ;
+        }
+        if(l2 != null){
+            temp.next = l2 ;
+        }
+        return head;
+        
+    }
+    
+    public ListNode merge(ListNode[] lists, int start, int end){
+        if(start == end)
+            return lists[start];
+        if(start+1 == end)
+            return sort(lists[start], lists[end]);
+        int mid = start + (end-start)/2;
+        ListNode firstHalf = merge(lists, start, mid);
+        ListNode secondHalf = merge(lists, mid+1, end);
+        return sort(firstHalf, secondHalf);
+    }
     public ListNode mergeKLists(ListNode[] lists) {
         if(lists.length == 0)
             return null;
-        PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
-        for(int i=0 ; i <lists.length ; i++){
-            ListNode temp = lists[i] ;
-            while(temp != null){
-                queue.add(temp.val);
-                temp=temp.next;
-            }
-        }
-        if(queue.isEmpty())
-            return null;
-        ListNode ans = new ListNode();
-        ListNode result = ans;
-        ans.val = queue.poll();
-        int size = queue.size();
-        for(int i=0 ; i<size ; i++){
-            ListNode t = new ListNode();
-            t.val = queue.poll();
-            ans.next = t ;
-            ans = t ;
-        }
-        return result;
+        if(lists.length == 1)
+            return lists[0];
+        return merge(lists, 0, lists.length-1);
     }
 }
