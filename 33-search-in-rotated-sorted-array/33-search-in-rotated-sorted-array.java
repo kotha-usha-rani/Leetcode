69ba5c08c23767087d7f905b89728e5e8966a6ed
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-13 16:29:45 +0530

    Time: 0 ms (100.00%), Space: 42 MB (57.36%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,82 +1,48 @@
 class Solution {
-    //Method 2
-   public int findIndex(int[] arr, int start, int end){
-       while(start <= end){
-           int mid = start + (end-start)/2;
-           if(mid < end && arr[mid] > arr[mid+1])
-               return mid;
-           if(mid>0 && arr[mid-1] > arr[mid])
-               return mid-1;
-           if(arr[mid] < arr[start])
-               end = mid-1;
-           else
-               start = mid+1;
-       }
-       return -1;
-   }
-    
-     //Method 2
-    public int binarySearch(int[] arr, int start, int end, int target){
+    public int find(int[] arr, int start, int end){
+		int mid;
+		while(start <= end){
+			mid = start + (end-start)/2;
+			if(mid+1 <= end && arr[mid] > arr[mid+1]){
+				return mid;
+			}
+			else{
+				if(mid-1 >= 0 && arr[mid] > arr[mid-1]){
+					if(arr[mid] < arr[arr.length-1])
+						end = mid-1;
+					else
+						start = mid+1;
+				}
+				else
+					return mid-1;
+			}
+		}
+		return -1;
+	}
+    public int findIndex(int[] arr, int start, int end, int target){
+        int mid;
         while(start <= end){
-            int mid = start + (end-start)/2;
+            mid = start + (end-start)/2;
             if(arr[mid] == target)
                 return mid;
-            if(target < arr[mid]){
+            if(arr[mid] > target)
                 end = mid-1;
-            }
             else
                 start = mid+1;
         }
         return -1;
     }
     
-    //Method 3
-    public int findTarget(int[] arr, int start, int end, int target){
-        while(start <= end){
-            int mid = start + (end-start)/2;
-            if(arr[mid] == target)
-                return mid;
-            if(arr[start] <= arr[mid]){
-                if(target < arr[mid] && target >= arr[start])
-                    end = mid-1;
-                else
-                    start = mid+1;
-            }
-            else {
-                if(target > arr[mid] && target <= arr[end])
-                    start = mid+1;
-                else
-                    end = mid-1;
-            }
-        }
-        return -1;
-    }
     public int search(int[] nums, int target) {
-        //Method 1 Linear search
-        // for(int i=0 ; i<nums.length ; i++){
-        //     if(nums[i] == target)
-        //         return i;
-        // }
-        // return -1;
-        
-        
-        //Method 2 Direct binary search is not possible
-        //therefore find the index where the array was rotated
-        //left of pivot is sorted in ascending order, right of pivot is sorted in                       ascending order as well, search in one part of the array
-        // int index = findIndex(nums, 0, nums.length-1);
-        // if(index == -1)
-        //     return binarySearch(nums, 0, nums.length-1, target);
-        // if(nums[index] == target)
-        //     return index;
-        // else if(target >= nums[0])
-        //     return binarySearch(nums, 0, index-1, target);
-        // else
-        //     return binarySearch(nums, index+1, nums.length-1, target);
-        
-        
-        //Method 3, O(logn) time complexity
-        //one part of the array is strictly increasing, if target is not in strictly increasing order, it might be in the other half, if the target if to be found in non-increasing order, we will be left with the half that has two elemenets in it, target might be found among the two
-        return findTarget(nums, 0, nums.length-1, target);
-            
+        int pivot = find(nums, 0, nums.length-1);
+        int ans ;
+        if(pivot != -1){
+            ans = findIndex(nums, 0, pivot, target);
+            if(ans == -1)
+                ans = findIndex(nums, pivot+1, nums.length-1, target);
+            return ans;
+        }
+        else 
+            return findIndex(nums, 0, nums.length-1, target);
     }
 }
