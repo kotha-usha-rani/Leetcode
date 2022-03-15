6cfd790c6bb2df650d19ace765d96f49e1b699c6
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-15 15:39:05 +0530

    Time: 1 ms (72.82%), Space: 43.1 MB (41.17%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -9,65 +9,18 @@
  * }
  */
 class Solution {
-    public ListNode recursive(ListNode t1, ListNode t2){
-        if(t1 == null)
-            return t2 ;
-        if(t2 == null)
-            return t1 ;
-        if(t1.val <= t2.val){
-            t1.next = recursive(t1.next, t2);
-            return t1;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null)
+            return list2;
+        if(list2 == null)
+            return list1;
+        if(list1.val <= list2.val){
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
         }
         else{
-            t2.next= recursive(t1, t2.next);
-            return t2;
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
         }
-            
-    }
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        //Method 1 iterative
-        // if(list1 == null && list2 == null)
-        //     return null;
-        // if(list1 == null)
-        //     return list2;
-        // if(list2 == null)
-        //     return list1;
-        // ListNode t1 =null , t2 = null;
-        // if(list1.val <= list2.val){
-        //     t1 = list1; t2 = list2;
-        // }
-        // else{
-        //     t1 = list2; t2 = list1 ;
-        // }
-        // ListNode prev = t1, newHead = t1 ;
-        // while(t1 != null && t2 != null){
-        //     if(t1.val <= t2.val){
-        //         prev = t1 ;
-        //         t1 = t1.next;
-        //     }
-        //     else{
-        //         ListNode temp = t2;
-        //         t2 = t2.next;
-        //         temp.next = t1;
-        //         prev.next = temp;
-        //         prev = temp;
-        //     }
-        // }
-        // if(t2 != null){
-        //     prev.next = t2;
-        // }
-        // return newHead;
-        
-        //Method 2 recursive
-        
-        return recursive(list1, list2);
     }
 }
-
-
-
-
-
-
-
-
