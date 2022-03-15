a062c10009362c4dbd0739ed004e943c78b3b0c2
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-15 21:33:39 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (72.14%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -15,6 +15,8 @@ class Solution {
             return list2;
         if(list2 == null)
             return list1;
+        
+        //Method 1 recursive
         // if(list1.val <= list2.val){
         //     list1.next = mergeTwoLists(list1.next, list2);
         //     return list1;
@@ -24,33 +26,57 @@ class Solution {
         //     return list2;
         // }
         
-        ListNode head = null, prev = null, l1=list1, l2=list2;
-        if(l1.val <= l2.val){
-            head = l1;
-            prev = l1;
-            l1 = l1.next;
+        
+        
+        //Method 2 iterative
+        // ListNode head = null, prev = null, l1=list1, l2=list2;
+        // if(l1.val <= l2.val){
+        //     head = l1;
+        //     prev = l1;
+        //     l1 = l1.next;
+        // }
+        // else{
+        //     head = l2;
+        //     prev = l2;
+        //     l2 = l2.next;
+        // }
+        // while(l1 != null && l2 != null){
+        //     if(l1.val <= l2.val){
+        //         prev.next = l1;
+        //         prev = l1;
+        //         l1 = l1.next;
+        //     }
+        //     else{
+        //         prev.next = l2;
+        //         prev = l2;
+        //         l2 = l2.next;
+        //     }
+        // }
+        // if(l1 != null)
+        //     prev.next = l1;
+        // if(l2 != null)
+        //     prev.next = l2;
+        // return head;
+        
+        //better code
+        ListNode l1 = list1, l2 = list2, temp = null, prev;
+        if(l1.val > l2.val){
+            temp = l1;
+            l1 = l2;
+            l2 = temp;
         }
-        else{
-            head = l2;
-            prev = l2;
-            l2 = l2.next;
-        }
+        ListNode head = l1;
         while(l1 != null && l2 != null){
-            if(l1.val <= l2.val){
-                prev.next = l1;
-                prev = l1;
+            prev = null;
+            while(l1 != null && l1.val <= l2.val){
+                prev = l1; 
                 l1 = l1.next;
             }
-            else{
-                prev.next = l2;
-                prev = l2;
-                l2 = l2.next;
-            }
-        }
-        if(l1 != null)
-            prev.next = l1;
-        if(l2 != null)
             prev.next = l2;
+            temp = l1;
+            l1 = l2;
+            l2 = temp;
+        }
         return head;
     }
 }
