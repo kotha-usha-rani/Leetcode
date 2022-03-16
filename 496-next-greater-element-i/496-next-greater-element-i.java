70c18976b0fb9ab46a3f513abd2fd902339f9abf
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-16 09:10:41 +0530

    Time: 2 ms (97.96%), Space: 42.1 MB (74.68%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,44 +1,21 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        //Method 1 using 2 loops
-        // int[] ans = new int[nums1.length];
-        // int k=0;
-        // for(int i=0 ; i<nums1.length ; i++){
-        //     for(int j=0 ; j<nums2.length ; j++){
-        //         if(nums1[i] == nums2[j]){
-        //             j=j+1;
-        //             while(j<nums2.length){
-        //                 if(nums1[i] < nums2[j]){
-        //                     ans[k++] = nums2[j];
-        //                     break;
-        //                 }
-        //                 j++;
-        //             }
-        //             if(j==nums2.length)
-        //                 ans[k++] = -1;
-        //             break;
-        //         }
-        //     }
-        // }
-        // return ans;
-        
-        //Method 2 sort the second array, use binary search to find next greater element
-        
-        Stack<Integer> smaller_Index = new Stack<>();
-        HashMap<Integer, Integer> nextGreater = new HashMap<>();
-        for(int ele : nums2){     
-            while(!smaller_Index.isEmpty() && smaller_Index.peek() < ele){
-                nextGreater.put(smaller_Index.pop(), ele);                    
+        Stack<Integer> stack = new Stack<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        stack.push(0);
+        for(int i=1 ; i<nums2.length ; i++){
+            while(!stack.isEmpty() && nums2[i] > nums2[stack.peek()]){
+                map.put(nums2[stack.pop()], nums2[i]);
             }
-            smaller_Index.push(ele);
+            stack.push(i);
         }
-        while(!smaller_Index.isEmpty()){
-            nextGreater.put(smaller_Index.pop(), -1);           
+        while(!stack.isEmpty()){
+            map.put(nums2[stack.pop()], -1);
         }
         int[] ans = new int[nums1.length];
         int k=0;
-        for(int ele : nums1){
-            ans[k++] = nextGreater.get(ele) ;
+        for(int i=0 ; i<nums1.length ; i++){
+            ans[k++] = map.get(nums1[i]);
         }
         return ans;
     }
