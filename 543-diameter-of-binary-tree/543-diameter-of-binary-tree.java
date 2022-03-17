a55aaca31e8c90fcff523050d88a9c6b40502bbb
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-17 15:52:37 +0530

    Time: 2 ms (16.42%), Space: 43.2 MB (43.78%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -24,8 +24,39 @@ class Solution {
         dia = Math.max(dia, left+right+1);
         return 1 + Math.max(left, right);
     }
+    
+    public Triplet diaUsingTriplet(TreeNode root){
+        if(root == null)
+            return new Triplet(0,0);
+        Triplet left = diaUsingTriplet(root.left);
+        Triplet right = diaUsingTriplet(root.right);
+        return new Triplet(Math.max(left.height + right.height + 1, Math.max(left.maxDia, right.maxDia)),                              1 + Math.max(left.height, right.height));
+        
+    }
     public int diameterOfBinaryTree(TreeNode root) {
-        int height = finddiameter(root);
-        return dia-1;
+        //method 1 using dia as global variable
+        
+        // int height = finddiameter(root);
+        // return dia-1;
+        
+        //using triplet class
+        if(root == null)
+            return 0;
+        return diaUsingTriplet(root).maxDia-1;
     }
 }
+class Triplet{
+    int maxDia;
+    int height;
+    Triplet(int maxDia, int height){
+        this.maxDia = maxDia;
+        this.height = height;
+    }
+}
+
+
+
+
+
+
+
