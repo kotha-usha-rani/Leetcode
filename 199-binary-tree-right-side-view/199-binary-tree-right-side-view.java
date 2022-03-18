474d2875d9c37ad096c0005d74d7c292d5605cac
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-18 11:58:04 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (75.03%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -18,11 +18,7 @@ class Solution {
     public void getView(TreeNode root, int level, List<Integer> ans){
         if(root == null)
             return;
-        if(ans.size() > 0){
-            if(level > ans.size()-1)
-                ans.add(root.val);
-        }
-        else
+        if(level > ans.size())
             ans.add(root.val);
         getView(root.right, level+1, ans);
         getView(root.left, level+1, ans);
@@ -31,7 +27,7 @@ class Solution {
         List<Integer> ans = new ArrayList<>();
         if(root == null)
             return ans;
-        getView(root, 0, ans);
+        getView(root, 1, ans);
         return ans;
     }
 }
