eaef04763a3af1d8588b284ad7d40cfc365d7a2e
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-19 05:13:57 +0530

    Time: 2 ms (34.04%), Space: 42.7 MB (52.44%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -28,35 +28,74 @@ class Solution {
         if(root == null)
             return new ArrayList<>();
         List<List<Integer>> ans = new ArrayList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
+        
+        //Queue, arraylists
+        
+        // Queue<TreeNode> queue = new LinkedList<>();
+        // ArrayList<Integer> temp = new ArrayList<>();
+        // int flag = 0 ;
+        // queue.add(root);
+        // queue.add(null);
+        // while(!queue.isEmpty()){
+        //     TreeNode curr = queue.remove();
+        //     if(curr != null){
+        //         temp.add(curr.val);
+        //         if(curr.left != null)
+        //             queue.add(curr.left);
+        //         if(curr.right != null)
+        //             queue.add(curr.right);
+        //     }
+        //     else{
+        //         if(flag == 0){
+        //             ans.add(temp);
+        //             flag = 1; 
+        //         }
+        //         else{
+        //             reverse(temp);
+        //             ans.add(temp);
+        //             flag = 0;
+        //         }
+        //         temp = new ArrayList<>(); 
+        //         if(queue.isEmpty())
+        //            break;
+        //         queue.add(null);
+        //     }
+        // }
+        // return ans;
+        
+        //two stacks
+        
+        Stack<TreeNode> st1 = new Stack<>();
+        Stack<TreeNode> st2 = new Stack<>();
         ArrayList<Integer> temp = new ArrayList<>();
-        int flag = 0 ;
-        queue.add(root);
-        queue.add(null);
-        while(!queue.isEmpty()){
-            TreeNode curr = queue.remove();
-            if(curr != null){
-                temp.add(curr.val);
-                if(curr.left != null)
-                    queue.add(curr.left);
-                if(curr.right != null)
-                    queue.add(curr.right);
-            }
-            else{
-                if(flag == 0){
-                    ans.add(temp);
-                    flag = 1; 
+        st2.push(root);
+        int flag = 0;
+        while(!st1.isEmpty() || !st2.isEmpty()){
+            TreeNode curr;
+            if(flag == 0){
+                while(!st2.isEmpty()){
+                    curr = st2.pop();
+                    temp.add(curr.val);
+                    if(curr.left != null)
+                        st1.push(curr.left);
+                    if(curr.right != null)
+                        st1.push(curr.right);
                 }
-                else{
-                    reverse(temp);
-                    ans.add(temp);
-                    flag = 0;
-                }
-                temp = new ArrayList<>(); 
-                if(queue.isEmpty())
-                   break;
-                queue.add(null);
+                flag = 1;
             }
+            else if(flag == 1){
+                while(!st1.isEmpty()){
+                    curr = st1.pop();
+                    temp.add(curr.val);
+                    if(curr.right != null)
+                        st2.push(curr.right);
+                    if(curr.left != null)
+                        st2.push(curr.left);
+                }
+                flag = 0;
+            }
+            ans.add(temp);
+            temp = new ArrayList<>();
         }
         return ans;
     }
