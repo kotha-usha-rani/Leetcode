a9332cfb7ae71c93f349694e4e1b3bf64b26fcb2
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-19 08:24:00 +0530

    Time: 1 ms (87.02%), Space: 42.6 MB (52.44%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -15,6 +15,24 @@
  */
 class Solution {
     
+    public void getZigZag(TreeNode root, int level, List<List<Integer>> ans){
+        if(root == null)
+            return;
+        if(ans.size()-1 < level){
+            ArrayList<Integer> temp = new ArrayList<>();
+            temp.add(root.val);
+            ans.add(temp);
+        }
+        else{
+            if(level%2 != 0)
+                ans.get(level).add(0,root.val);
+            else
+                ans.get(level).add(root.val);
+        }            
+        getZigZag(root.left, level+1, ans);
+        getZigZag(root.right, level+1, ans);
+    }
+    
     public void reverse(ArrayList<Integer> arr){
         int i=0, j=arr.size()-1;
         while(i<j){
@@ -65,38 +83,43 @@ class Solution {
         
         //two stacks
         
-        Stack<TreeNode> st1 = new Stack<>();
-        Stack<TreeNode> st2 = new Stack<>();
-        ArrayList<Integer> temp = new ArrayList<>();
-        st2.push(root);
-        int flag = 0;
-        while(!st1.isEmpty() || !st2.isEmpty()){
-            TreeNode curr;
-            if(flag == 0){
-                while(!st2.isEmpty()){
-                    curr = st2.pop();
-                    temp.add(curr.val);
-                    if(curr.left != null)
-                        st1.push(curr.left);
-                    if(curr.right != null)
-                        st1.push(curr.right);
-                }
-                flag = 1;
-            }
-            else if(flag == 1){
-                while(!st1.isEmpty()){
-                    curr = st1.pop();
-                    temp.add(curr.val);
-                    if(curr.right != null)
-                        st2.push(curr.right);
-                    if(curr.left != null)
-                        st2.push(curr.left);
-                }
-                flag = 0;
-            }
-            ans.add(temp);
-            temp = new ArrayList<>();
-        }
+        // Stack<TreeNode> st1 = new Stack<>();
+        // Stack<TreeNode> st2 = new Stack<>();
+        // ArrayList<Integer> temp = new ArrayList<>();
+        // st2.push(root);
+        // int flag = 0;
+        // while(!st1.isEmpty() || !st2.isEmpty()){
+        //     TreeNode curr;
+        //     if(flag == 0){
+        //         while(!st2.isEmpty()){
+        //             curr = st2.pop();
+        //             temp.add(curr.val);
+        //             if(curr.left != null)
+        //                 st1.push(curr.left);
+        //             if(curr.right != null)
+        //                 st1.push(curr.right);
+        //         }
+        //         flag = 1;
+        //     }
+        //     else if(flag == 1){
+        //         while(!st1.isEmpty()){
+        //             curr = st1.pop();
+        //             temp.add(curr.val);
+        //             if(curr.right != null)
+        //                 st2.push(curr.right);
+        //             if(curr.left != null)
+        //                 st2.push(curr.left);
+        //         }
+        //         flag = 0;
+        //     }
+        //     ans.add(temp);
+        //     temp = new ArrayList<>();
+        // }
+        // return ans;
+        
+        //preorder recursive
+        
+        getZigZag(root, 0, ans);
         return ans;
     }
 }
