d99cc97b5c3f82bea9d002fb9d2160ffd8b63e86
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-20 17:53:25 +0530

    Time: 1 ms (42.78%), Space: 42.6 MB (20.07%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -15,51 +15,25 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
+        //iterative
         if(root == null)
             return new ArrayList<>();
-        // iterative
-        // Stack<TreeNode> stack = new Stack<>();
-        // List<Integer> ans = new ArrayList<>();
-        // stack.push(root);
-        // while(!stack.isEmpty()){
-        //     TreeNode temp = stack.peek();
-        //     while(temp.left != null){
-        //         stack.push(temp.left);
-        //         temp = temp.left;
-        //     }
-        //     while(!stack.isEmpty()){
-        //         ans.add(stack.peek().val);
-        //         if(stack.peek().right != null){
-        //             stack.push(stack.pop().right);
-        //             break;
-        //         }
-        //         stack.pop();
-        //     }
-        // }
-        // return ans;
-        
-        //recursive
-        // List<Integer> ans = inorderTraversal(root.left);
-        // ans.add(root.val);
-        // ans.addAll(inorderTraversal(root.right));
-        // return ans;
-        
-        
-        //iteratice better code
-        TreeNode curr = root;
-        Stack<TreeNode> stack = new Stack<>();
         List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
         while(!stack.isEmpty() || curr != null){
-            if(curr != null){
+            while(curr != null){
                 stack.push(curr);
                 curr = curr.left;
             }
-            else{
-                curr = stack.pop();
-                ans.add(curr.val);
-                curr = curr.right;
-            }
+            curr = stack.pop();
+            ans.add(curr.val);
+            curr = curr.right;
         }
         return ans;
     }
 }
+
+
+
+
