28bf30b7721944c2acda98df0a81834199224e0b
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-20 09:53:51 +0530

    Time: 0 ms, Memory: 41.4 MB - LeetHub

---
+++ k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int kthGrammar(int n, int k) {
+        if(n==1)
+            return 0;
+        int ans = kthGrammar(n-1, k/2 + k%2);
+        if(ans == 0){
+            if(k%2!=0)
+                return 0;
+            else
+                return 1;
+        }
+        else{
+            if(k%2!=0)
+                return 1;
+            else
+                return 0;
+        }
+    }
+}
