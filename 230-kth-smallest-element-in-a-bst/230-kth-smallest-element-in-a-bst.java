95cf25d92e566dc8993631b64b33c52ef2f84005
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-26 09:18:16 +0530

    Time: 1 ms (71.04%), Space: 45.8 MB (25.58%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -16,17 +16,35 @@
 class Solution {
     public int count = 0 ;
     public int ans = -1;
+    
+    public void findLar(TreeNode root, int k, int[] ans){
+        if(root == null)
+            return;
+        findLar(root.left, k, ans);
+        ans[0]++;
+        if(ans[0] == k){
+            ans[1] = root.val;
+            return;
+        }
+        findLar(root.right, k, ans);
+            
+    }
     public int kthSmallest(TreeNode root, int k) {
         //left root right
-        if(root == null)
-            return -1;
-        int left = kthSmallest(root.left, k);
-        count++;
-        if(count == k){
-            ans = root.val;
-            return ans;
-        }
-        int right = kthSmallest(root.right, k);
-        return ans;
+        // if(root == null)
+        //     return -1;
+        // int left = kthSmallest(root.left, k);
+        // count++;
+        // if(count == k){
+        //     ans = root.val;
+        //     return ans;
+        // }
+        // int right = kthSmallest(root.right, k);
+        // return ans;
+        
+        //temp array to store count and root value
+        int[] ans = new int[2];
+        findLar(root, k, ans);
+        return ans[1];
     }
 }
