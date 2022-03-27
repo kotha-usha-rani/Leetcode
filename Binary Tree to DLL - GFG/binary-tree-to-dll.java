3c3af8cc100a40761b2fc9728aaf386c01283549
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-27 06:40:02 +0530

    Added solution - LeetHub

---
+++ Binary Tree to DLL - GFG/binary-tree-to-dll.java
@@ -0,0 +1,183 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+	Node left, right;
+	int data;
+	
+	Node(int d)
+	{
+		data = d;
+		left = right = null;
+	}
+	
+}
+class BT_To_DLL
+{
+    
+    static Node buildTree(String str)throws IOException{
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+
+	void inorder(Node node)
+	{
+		if(node==null)
+			return ;
+		else
+			inorder(node.left);
+			System.out.print(node.data + " ");
+			inorder(node.right);
+	}
+	public static void printList(Node head) 
+    {
+		Node prev = head;
+        while (head != null) 
+        {
+            System.out.print(head.data + " ");
+			prev = head;
+            head = head.right;
+        }
+		
+		System.out.println();
+		while(prev != null)
+		{
+			System.out.print(prev.data+" ");
+			prev = prev.left;
+		}
+    }
+	public static void main(String args[])  throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Solution g = new Solution();
+			    
+			    Node ans = g.bToDLL(root);
+			    printList(ans);
+                t--;
+                System.out.println();
+	        }
+			
+		
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* class Node
+class Node
+{
+	Node left, right;
+	int data;
+	
+	Node(int d)
+	{
+		data = d;
+		left = right = null;
+	}
+	
+}*/
+
+//This function should return head to the DLL
+
+class Solution
+{
+    //Function to convert binary tree to doubly linked list and return it.
+    Pair helper(Node root){
+        if(root == null)
+            return new Pair(null,null);
+        Pair left = helper(root.left);
+        root.left = left.tail;
+        if(left.tail != null)
+            left.tail.right = root;
+        else
+            left.head = root;
+            
+        Pair right = helper(root.right);
+        root.right = right.head;
+        if(right.head != null)
+            right.head.left = root;
+        else
+            right.tail = root;
+        return new Pair(left.head, right.tail);
+    }
+    Node bToDLL(Node root)
+    {
+	    Pair ans = helper(root);
+	    return ans.head ;
+    }
+}
+class Pair{
+    Node head;
+    Node tail;
+    Pair(Node head, Node tail){
+        this.head = head;
+        this.tail = tail;
+    }
+}
