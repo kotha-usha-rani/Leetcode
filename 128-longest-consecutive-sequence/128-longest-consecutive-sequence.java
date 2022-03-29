a57699ed12a2bf3883392e63448e2cd21d3394f5
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-29 08:58:39 +0530

    Time: 529 ms (21.03%), Space: 147.5 MB (21.74%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -2,30 +2,48 @@ class Solution {
     public int longestConsecutive(int[] nums) {
         if(nums.length == 0)
             return 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        //using hashmap, but removing elements takes extra time 
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for(int i=0 ; i<nums.length ; i++){
+        //     map.put(nums[i], i);
+        // }
+        // int count = 1;
+        // int max = 1;
+        // for(int i=0 ; i<nums.length ; i++){
+        //     if(map.containsKey(nums[i])){
+        //         int k=1 ; count = 1 ;
+        //         Integer ans = map.get(nums[i]-k);
+        //         while(ans != null){
+        //             count++;
+        //             ans = map.remove(nums[i]-k++);
+        //             ans = map.get(nums[i]-k);                
+        //         }
+        //         k=1 ;
+        //         ans = map.get(nums[i]+k);
+        //         while(ans != null){
+        //             count++;
+        //             ans = map.get(nums[i]+k++);
+        //             ans = map.get(nums[i]+k);
+        //         }
+        //         max = Math.max(count, max);
+        //     }
+        // }
+        // return max;
+        
+        //method 2 using hashset, start counting from min number so that we do not count the numbers again and again
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0 ; i<nums.length ; i++)
+            set.add(nums[i]);
+        int count = 0;
+        int max = 1 ;
         for(int i=0 ; i<nums.length ; i++){
-            map.put(nums[i], i);
-        }
-        int count = 1;
-        int max = 1;
-        for(int i=0 ; i<nums.length ; i++){
-            if(map.containsKey(nums[i])){
-                int k=1 ; count = 1 ;
-                Integer ans = map.get(nums[i]-k);
-                while(ans != null){
+            if(!set.contains(nums[i]-1)){
+                count = 1 ; int k=1;
+                while(set.contains(nums[i]+k++))
                     count++;
-                    ans = map.remove(nums[i]-k++);
-                    ans = map.get(nums[i]-k);                
-                }
-                k=1 ;
-                ans = map.get(nums[i]+k);
-                while(ans != null){
-                    count++;
-                    ans = map.get(nums[i]+k++);
-                    ans = map.get(nums[i]+k);
-                }
-                max = Math.max(count, max);
             }
+            max = Math.max(count, max);
         }
         return max;
     }
