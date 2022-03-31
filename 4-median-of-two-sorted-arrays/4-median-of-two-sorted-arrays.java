fa99f2eea71e8a9471063c0970c2ec69a62707cd
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-31 08:22:16 +0530

    Time: 1 ms (100.00%), Space: 43.4 MB (84.28%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -10,18 +10,69 @@ class Solution {
         else
             return (double)(m+n)/2.0;
     }
+    
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // if(nums1.length == 0)
+        //     return findM(nums2);
+        // if(nums2.length == 0)
+        //     return findM(nums1);
+        // int[] arr = new int[nums1.length + nums2.length];
+        // int i=0;
+        // for(; i<nums1.length ; i++)
+        //     arr[i] = nums1[i];
+        // for(i=0;i<nums2.length ; i++)
+        //     arr[i+nums1.length] = nums2[i];
+        // Arrays.sort(arr);
+        // return findM(arr);
+        
         if(nums1.length == 0)
             return findM(nums2);
         if(nums2.length == 0)
             return findM(nums1);
-        int[] arr = new int[nums1.length + nums2.length];
-        int i=0;
-        for(; i<nums1.length ; i++)
-            arr[i] = nums1[i];
-        for(i=0;i<nums2.length ; i++)
-            arr[i+nums1.length] = nums2[i];
-        Arrays.sort(arr);
-        return findM(arr);
+        
+        int i=0, j=0, count=-1, mid=0, midP = 0, m = (nums1.length + nums2.length)/2;
+        
+        while(i<nums1.length && j<nums2.length){
+            count++;
+            if(nums1[i] <= nums2[j]){
+                if(count == m){
+                    mid = nums1[i];
+                    break;
+                }
+                midP = nums1[i];
+                i++;
+            }
+            else{
+                if(count == m){
+                    mid = nums2[j];
+                    break;
+                }
+                midP = nums2[j];
+                j++;
+            }
+        }
+        while(count < m && i<nums1.length){
+            count++;
+            if(count == m){
+                mid = nums1[i];
+                break;
+            }
+            midP = nums1[i];
+            i++;
+        }
+        while(count < m && j<nums2.length){
+            count++;
+            if(count == m){
+                mid = nums2[j];
+                break;
+            }
+            midP = nums2[j];
+            j++;
+        }
+        
+        if((nums1.length + nums2.length)%2 != 0)
+            return mid;
+        else
+            return (double)(mid+midP)/2.0;
     }
 }
