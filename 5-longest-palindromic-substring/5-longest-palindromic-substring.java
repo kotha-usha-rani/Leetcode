650e26b05a21167a481044b9bd78af3db572fe7a
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-31 12:41:29 +0530

    Time: 45 ms (64.55%), Space: 43.2 MB (61.81%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,32 +1,38 @@
 class Solution {
+    public void findSE(String s, int j, int k, Pair SE){
+        while(j-1>-1 && k+1<s.length() && s.charAt(j-1) == s.charAt(k+1)){
+                j--;k++;
+            }
+            if(k-j+1 > SE.maxE-SE.maxS+1){
+                SE.maxS = j;
+                SE.maxE = k;
+            }
+    }
     public String longestPalindrome(String s) {
         if(s.length() <= 1)
             return s;
         int maxS = 0, maxE = 0, j=0, k=0;
+        Pair SE = new Pair(0, 0);
         for(int i=1 ; i<s.length() ; i++){
             j=i; k=i;
-            while(j-1>-1 && k+1<s.length() && s.charAt(j-1) == s.charAt(k+1)){
-                j--;k++;
-            }
-            if(k-j+1 > maxE-maxS+1){
-                maxS = j;
-                maxE = k;
-            }
+            findSE(s, j, k, SE);
         }
         for(int i=0 ; i<s.length()-1 ; i++){
             if(s.charAt(i) != s.charAt(i+1))
                 continue;
             else{
                 j=i; k=i+1;
-                 while(j-1>-1 && k+1<s.length() && s.charAt(j-1) == s.charAt(k+1)){
-                     j--;k++;
-                 }
-                 if(k-j+1 > maxE-maxS+1){
-                    maxS = j;
-                    maxE = k ;
-                 }
+                findSE(s, j, k, SE);
             }
         }
-        return s.substring(maxS, maxE+1);
+        return s.substring(SE.maxS, SE.maxE+1);
+    }
+}
+class Pair{
+    int maxS;
+    int maxE;
+    Pair(int maxS, int maxE){
+        this.maxS = maxS;
+        this.maxE = maxE;
     }
 }
