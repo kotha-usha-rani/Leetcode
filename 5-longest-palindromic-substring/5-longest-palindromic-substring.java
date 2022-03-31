b1e56ae56bc559e0ba463f5661f2188a4e8e4035
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-31 13:47:57 +0530

    Time: 31 ms (84.41%), Space: 43.5 MB (54.18%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,38 +1,24 @@
 class Solution {
-    public void findSE(String s, int j, int k, Pair SE){
-        while(j-1>-1 && k+1<s.length() && s.charAt(j-1) == s.charAt(k+1)){
+    
+    public int findSE(String s, int j, int k){
+        while(j>=0 && k<s.length() && s.charAt(j) == s.charAt(k)){
                 j--;k++;
-            }
-            if(k-j+1 > SE.maxE-SE.maxS+1){
-                SE.maxS = j;
-                SE.maxE = k;
-            }
+        }
+        return k-j-1;
     }
+    
     public String longestPalindrome(String s) {
         if(s.length() <= 1)
             return s;
-        int maxS = 0, maxE = 0, j=0, k=0;
-        Pair SE = new Pair(0, 0);
-        for(int i=1 ; i<s.length() ; i++){
-            j=i; k=i;
-            findSE(s, j, k, SE);
-        }
-        for(int i=0 ; i<s.length()-1 ; i++){
-            if(s.charAt(i) != s.charAt(i+1))
-                continue;
-            else{
-                j=i; k=i+1;
-                findSE(s, j, k, SE);
+        int maxS = 0, maxE = 0;
+        for(int i=0 ; i<s.length() ; i++){
+            int odd = findSE(s, i, i);
+            int even = findSE(s, i, i+1);
+            if(maxE < Math.max(odd, even)){
+               maxS = odd > even ? i-odd/2 : i-even/2+1;
+               maxE = Math.max(odd, even);
             }
         }
-        return s.substring(SE.maxS, SE.maxE+1);
-    }
-}
-class Pair{
-    int maxS;
-    int maxE;
-    Pair(int maxS, int maxE){
-        this.maxS = maxS;
-        this.maxE = maxE;
+        return s.substring(maxS, maxS+maxE);
     }
 }
