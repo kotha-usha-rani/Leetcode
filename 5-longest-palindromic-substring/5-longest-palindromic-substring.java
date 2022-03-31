94f53400f509d65da39190255a5cca8cfc9b9a1f
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-03-31 12:31:55 +0530

    Time: 40 ms (73.10%), Space: 43.2 MB (61.81%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,29 +1,32 @@
 class Solution {
-        public int min = 0;
-        boolean checkPalindrome(String S, int start, int end){
-    	int i=start ; int j=end;
-        while(i<j){
-            if(S.charAt(i) != S.charAt(j))
-                return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
-
     public String longestPalindrome(String s) {
-        String ans = "";
-        int max = 0 ;
-        for(int i=0 ; i<s.length() ; i++){
-            for(int j=i ; j<s.length() ; j++){
-                int len = j-i+1;
-                if(len > max && checkPalindrome(s, i, j)){
-                    if(len > max)
-                        max = len;
-                    ans = s.substring(i,j+1);
-                }
+        if(s.length() <= 1)
+            return s;
+        int maxS = 0, maxE = 0, j=0, k=0;
+        for(int i=1 ; i<s.length() ; i++){
+            j=i; k=i;
+            while(j-1>-1 && k+1<s.length() && s.charAt(j-1) == s.charAt(k+1)){
+                j--;k++;
+            }
+            if(k-j+1 > maxE-maxS+1){
+                maxS = j;
+                maxE = k;
             }
         }
-        return ans ;
+        for(int i=0 ; i<s.length()-1 ; i++){
+            if(s.charAt(i) != s.charAt(i+1))
+                continue;
+            else{
+                j=i; k=i+1;
+                 while(j-1>-1 && k+1<s.length() && s.charAt(j-1) == s.charAt(k+1)){
+                     j--;k++;
+                 }
+                 if(k-j+1 > maxE-maxS+1){
+                    maxS = j;
+                    maxE = k ;
+                 }
+            }
+        }
+        return s.substring(maxS, maxE+1);
     }
 }
