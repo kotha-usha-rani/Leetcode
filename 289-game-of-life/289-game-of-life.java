f9f17ba09e58ab14417027d6d4eccac89c46a256
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-04-12 21:11:05 +0530

    Time: 1 ms (55.24%), Space: 42.3 MB (58.24%) - LeetHub

--- 289-game-of-life/289-game-of-life.java
+++ 289-game-of-life/289-game-of-life.java
@@ -1,33 +1,31 @@
 class Solution {
-    public int getCount(int[][] board, int[][] ans, int i, int j){
-        if(i>=0 && i<ans.length && j>=0 && j<ans[i].length)
-            return  board[i][j];
-        return 0;
-    }
     public void gameOfLife(int[][] board) {
-        int[][] ans = new int[board.length][board[0].length];
-        for(int i=0 ; i<ans.length ; i++){
-            for(int j=0 ; j<ans[i].length ; j++){
-                ans[i][j] = -1;
-            }
-        }
         int[] directions = new int[]{-1, -1, -1, 0, -1, 1, 0, 1, 1, 1, 1, 0, 1, -1, 0, -1};
         
         for(int i=0 ; i<board.length ; i++){
             for(int j=0 ; j<board[i].length ; j++){
                 int count = 0;
-                for(int k=0 ; k<directions.length ; k+=2)
-                    count += getCount(board, ans, i+directions[k], j+directions[k+1]);
+                for(int k=0 ; k<directions.length ; k+=2){
+                    int ti = i+directions[k], tj = j+directions[k+1];
+                    if(ti>=0 && ti<board.length && tj>=0 && tj<board[i].length){
+                        if(board[ti][tj] == 1 || board[ti][tj] == 4 || board[ti][tj] == 5)
+                            count += 1 ;
+                    }
+                }
                 if(board[i][j] == 1)
-                    ans[i][j] = (count<2 || count>3) ? 0 : 1; 
+                    board[i][j] = (count<2 || count>3) ? 4 : 5; 
                 else
-                    ans[i][j] = (count == 3) ? 1 : 0;
+                    board[i][j] = (count == 3) ? 3 : 2;
             }
         }
-        for(int i=0 ; i<ans.length ; i++){
-            for(int j=0 ; j<ans[i].length ; j++){
-                board[i][j] = ans[i][j];
+        for(int i=0 ; i<board.length ; i++){
+            for(int j=0 ; j<board[i].length ; j++){
+                if(board[i][j] == 2 || board[i][j] == 4)
+                    board[i][j] = 0;
+                else
+                    board[i][j] = 1;
             }
         }
     }
+            
 }
