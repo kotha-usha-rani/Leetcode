e15491fdd3ab33eaf496904485e9aa46af059aa0
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-04-13 11:47:08 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (86.14%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -7,14 +7,13 @@ class Solution {
                 else{
                     i--;j--;
                 } 
-                continue;
+                continue;    // to avoid index out of bounds, if you don't want to use continue, use checks (i>=0 || j>=0) in every if conditions below
             }
             if(s.charAt(i) == '#'){
                 count1++; i--;
             }
             else if(count1 > 0){
                 i--; count1--;
-                
             }
             if(t.charAt(j) == '#'){
                 count2++; j--;
@@ -23,6 +22,10 @@ class Solution {
                 j--; count2--;
             }
         }
+        //to check if any string has leftover characters Note: comparisions in both the strings might be exhausted, or one of the string's is exhausted
+        //therefore check for the string which has characters to compare "remember that presence of '#' will cancel out the alphabets and might lead to equal strings" 
+        //if there is a string with alphabets without "#" after them will not be equal strings (this will not apply to the above while loop) 
+        //hence the need of this checks is to cancel out all the remaining alphabets
         if(i>=0){
             if(s.charAt(i) != '#'){
                 if(count1 > 0) count1--;
@@ -39,6 +42,8 @@ class Solution {
             else count2++;
             j--;
         }
+        
+        //if all the characters are equal
         return true;
     }
 }
