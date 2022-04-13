369bb0f08de18120ec1dc874c9daff48ee9ea153
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-04-13 23:36:15 +0530

    Time: 7 ms (28.54%), Space: 50.1 MB (86.20%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.java
+++ 997-find-the-town-judge/997-find-the-town-judge.java
@@ -1,31 +1,20 @@
 class Solution {
-    public boolean search(int[][] trust, int i, int j){
-        for(int k=0 ; k<trust.length ; k++){
-            if(trust[k][0] == i && trust[k][1] == j)
-                return true;
-        }
-        return false;
-    }
     public int findJudge(int n, int[][] trust) {
-        Stack<Integer> stack = new Stack<>();
-        for(int i=1 ; i<=n ; i++)
-            stack.push(i);
-        while(stack.size() > 1){
-            int i = stack.pop();
-            int j = stack.pop();
-            if(search(trust, i, j))
-                stack.push(j);
-            else
-                stack.push(i);
-        }
-        int possibleJudge = stack.pop();
-        int count = 0;
+        int[][] graph = new int[n+1][n+1];
         for(int i=0 ; i<trust.length ; i++){
-            if(trust[i][0] == possibleJudge)
-                return -1;
-            if(trust[i][1] == possibleJudge)
-                count++;
+            graph[trust[i][0]][trust[i][1]] = 1;
         }
-        return (count == n-1) ? possibleJudge : -1;
+        for(int i=1 ; i<graph.length ; i++){
+            int count = 0 ;
+            for(int j=1 ; j<graph.length ; j++){
+                if(graph[i][j] == 1)
+                    break;
+                if(graph[j][i] == 1)
+                    count++;
+            }
+            if(count == n-1)
+                return i;
+        }
+        return -1;
     }
 }
