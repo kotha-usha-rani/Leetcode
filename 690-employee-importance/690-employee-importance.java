31ced4e49ac2073bfe16d8aafbfe68505567eaae
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-04-13 18:35:01 +0530

    Time: 11 ms (23.55%), Space: 43.4 MB (81.57%) - LeetHub

--- 690-employee-importance/690-employee-importance.java
+++ 690-employee-importance/690-employee-importance.java
@@ -9,15 +9,49 @@ class Employee {
 
 class Solution {
     public int getImportance(List<Employee> employees, int id) {
-        for(int i=0 ; i<employees.size() ; i++){
-            if(employees.get(i).id == id){
-                int count = employees.get(i).importance;
-                for(int j=0 ; j<employees.get(i).subordinates.size() ; j++){
-                    count += getImportance(employees, employees.get(i).subordinates.get(j));
+        //recursive
+        // for(int i=0 ; i<employees.size() ; i++){
+        //     if(employees.get(i).id == id){
+        //         int count = employees.get(i).importance;
+        //         for(int j=0 ; j<employees.get(i).subordinates.size() ; j++){
+        //             count += getImportance(employees, employees.get(i).subordinates.get(j));
+        //         }
+        //         return count;
+        //     }
+        // }
+        // return 0;
+        
+        
+        //iterative
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(id);
+        int count = 0;
+        while(!queue.isEmpty()){
+            int currId = queue.remove();
+            for(int i=0 ; i<employees.size() ; i++){
+                if(employees.get(i).id == currId){
+                    count += employees.get(i).importance;
+                    for(int j=0 ; j<employees.get(i).subordinates.size() ; j++){
+                        queue.add(employees.get(i).subordinates.get(j));
+                    }
+                    break;
                 }
-                return count;
             }
         }
-        return 0;
+        return count;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
