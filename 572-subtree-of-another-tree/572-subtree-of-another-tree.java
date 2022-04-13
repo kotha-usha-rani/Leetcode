2abd3b04d29ab3df20e572c6426d7e15346736d0
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-04-14 05:02:44 +0530

    Time: 66 ms (5.09%), Space: 42.6 MB (81.26%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.java
@@ -15,23 +15,40 @@
  */
 class Solution {
     public boolean checkIfMath(TreeNode root, TreeNode subRoot){
-        if(root == null && subRoot == null)
-            return true;
-        if(root == null || subRoot == null || root.val != subRoot.val)
-            return false;
-        return checkIfMath(root.left, subRoot.left) && checkIfMath(root.right, subRoot.right);
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> s2 = new Stack<>();
+        s1.push(root); s2.push(subRoot);
+        while(!s1.isEmpty() && !s2.isEmpty()){
+            TreeNode currRoot = s1.pop();
+            TreeNode currSubRoot = s2.pop();
+            if(currRoot != null && currSubRoot != null && currRoot.val != currSubRoot.val)
+                return false;
+            if(currRoot == null && currSubRoot != null || currRoot != null && currSubRoot == null)
+                return false;
+            if(currRoot != null){
+                s1.push(currRoot.left);
+                s1.push(currRoot.right);
+            }
+            if(currSubRoot != null){
+                s2.push(currSubRoot.left);
+                s2.push(currSubRoot.right);
+            }
+        }
+        return s1.isEmpty() && s2.isEmpty();
     }
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while(!queue.isEmpty()){
-            TreeNode curr = queue.remove();
+        if(root == null || subRoot == null)
+            return root == subRoot;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode curr = stack.pop();
             if(curr.val == subRoot.val && checkIfMath(curr, subRoot))
                 return true;
             if(curr.left != null)
-                queue.add(curr.left);
+                stack.push(curr.left);
             if(curr.right != null)
-                queue.add(curr.right);
+                stack.push(curr.right);
         }
         return false;
     }
