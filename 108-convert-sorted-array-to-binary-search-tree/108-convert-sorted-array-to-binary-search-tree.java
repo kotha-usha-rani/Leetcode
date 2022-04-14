2aa67f26c394cafbafb434ee492aec66be1b9f2b
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-04-14 09:51:44 +0530

    Time: 2 ms (27.45%), Space: 42.2 MB (88.80%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.java
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.java
@@ -17,25 +17,20 @@ class Solution {
     public TreeNode sortedArrayToBST(int[] nums) {
         Stack<Data> stack = new Stack<>();
         TreeNode head = new TreeNode(nums[(nums.length-1)/2]);
-        stack.push(new Data(head, 0, nums.length-1));
+        stack.push(new Data(head, 0, (nums.length-1)/2, nums.length-1));
         while(!stack.isEmpty()){
             Data top = stack.pop();
             if(top.start == top.end || top.prev == null)
                 continue;
             TreeNode prev = top.prev;
-            int mid = top.start + (top.end-top.start)/2;
-            int leftNodeIndex = top.start + (mid-1-top.start)/2;
-            int rightNodeIndex = mid+1 + (top.end-(mid+1))/2;
-            if(leftNodeIndex >= top.start && leftNodeIndex <= mid-1) 
-                prev.left = new TreeNode(nums[leftNodeIndex]);
-            else 
-                prev.left = null;
-            if(rightNodeIndex >= mid+1 && rightNodeIndex <= top.end) 
-                prev.right = new TreeNode(nums[rightNodeIndex]);
-            else 
-                prev.right = null;
-            stack.push(new Data(prev.left, top.start, mid-1));
-            stack.push(new Data(prev.right, mid+1, top.end));
+            int leftIndex = top.start + (top.mid-1-top.start)/2;
+            int rightIndex = top.mid+1 + (top.end-(top.mid+1))/2;
+            if(leftIndex >= top.start && leftIndex <= top.mid-1) 
+                prev.left = new TreeNode(nums[leftIndex]);
+            if(rightIndex >= top.mid+1 && rightIndex <= top.end) 
+                prev.right = new TreeNode(nums[rightIndex]);
+            stack.push(new Data(prev.left, top.start, leftIndex, top.mid-1));
+            stack.push(new Data(prev.right, top.mid+1, rightIndex, top.end));
         }
         return head;
     }
@@ -43,11 +38,13 @@ class Solution {
 class Data{
     TreeNode prev;
     int start;
+    int mid;
     int end;
     Data(){}
-    Data(TreeNode prev, int start, int end){
+    Data(TreeNode prev, int start, int mid, int end){
         this.prev = prev;
         this.start= start;
+        this.mid = mid;
         this.end = end;
     }
 }
