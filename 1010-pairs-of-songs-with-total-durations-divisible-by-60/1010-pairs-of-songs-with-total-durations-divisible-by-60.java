0397fa7978ba4289739d30311f1a9824781a75a5
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-04-14 17:27:02 +0530

    Time: 95 ms (5.07%), Space: 49.2 MB (91.26%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -1,17 +1,17 @@
 class Solution {
     public int numPairsDivisibleBy60(int[] time) {
         HashMap<Integer, Integer> map = new HashMap<>();
+        int max = time[0];
         for(int i=0 ; i<time.length ; i++){
             if(map.containsKey(time[i]))
                map.put(time[i], map.get(time[i])+1);
             else
                map.put(time[i], 1);
-        }
-        int max = time[0];
-        for(int i=1 ; i<time.length ; i++)
             max = Math.max(max, time[i]);
+        }
+        
         int count = 0;
-        for(int i=0 ; i<time.length ; i++){
+        for(int i=0 ; i<time.length-1 ; i++){
             if(map.get(time[i]) > 0){
                 int n=1 ;
                 map.put(time[i],map.get(time[i])-1);
