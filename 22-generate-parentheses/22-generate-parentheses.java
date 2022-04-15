6d97e9652c7041af5ecaf42861abcd74e6bf2ee7
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-04-15 23:19:20 +0530

    Time: 302 ms (5.14%), Space: 43.3 MB (73.92%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void helper(int n, StringBuilder sb, HashSet<String> set){
+        if(n==1){
+            set.add(new String(sb.toString()));
+            return;
+        }
+        for(int i=0 ; i<sb.length()+1 ; i++){
+            sb.insert(i, "()");
+            helper(n-1, sb, set);
+            sb.delete(i, i+2);
+        }
+    }
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        if(n==1){
+            ans.add("()");
+            return ans;
+        }
+        HashSet<String> set = new HashSet<>();
+        helper(n, new StringBuilder("()"), set);
+        for(String s : set)
+            ans.add(s);
+        return ans;
+    }
+}
