ee595cf14640797aaa1bce8c782a7c788fff38c4
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-04-16 07:49:45 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (92.89%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,5 +1,23 @@
 class Solution {
+    public int makeMove(int n){
+        if(n%2 == 0)
+            return n-1;
+        int i=n/2;
+        if(i%2 == 0)
+            i++;
+        for(; i>=1 ; i-=2){
+            if(n%i == 0)
+                return n-i;
+        }
+        return 1;
+    }
     public boolean divisorGame(int n) {
-        return (n&1) == 0;
+        while(n>1){
+            n = makeMove(n);
+            if(n==1)
+                return true;
+            n = makeMove(n);
+        }
+        return false;
     }
 }
