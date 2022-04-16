e44bd8d99b73eeac9c8d72c281f262ec98b75a77
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-04-16 10:27:13 +0530

    Time: 5 ms (13.38%), Space: 39.3 MB (81.37%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,27 +1,14 @@
 class Solution {
-    public int makeMove(int n){
-        if(n%2 == 0) {             //if n is even you are gonna be the winner so make sure that the opposite team member gets odd number
-            int i=n/2;     
-            for(; i>1 ; i-=2){        
-                if(n%i == 0 && (n-i)%2 != 0)
-                    return n-i;
-            }
-            return n-1;
-        } 
-        int i=(int)Math.floor(n/2);     // if n is odd you are gonna loose, so better not to waste time, end the game as soon as possible but getting the highest  
-        for(; i>1 ; i-=2){        //factor using which n gets reduced as much as possible
-            if(n%i == 0)
-                return n-i;
-        }
-        return n-1;
-    }
     public boolean divisorGame(int n) {
-        while(n>1){
-            n = makeMove(n);  //Alice turn
-            if(n==1)
-                return true;
-            n = makeMove(n);  //bobs turn
+        boolean[] dp = new boolean[10001];
+        for(int i=2 ; i<=n ; i++){          //start with 2 as n=1 is false anyways
+            for(int j=1 ; j<i ; j++){
+                if(i%j == 0 && !dp[i-j]){     // to check the factors of i and if i-j leads to an odd number to 
+                    dp[i] = true;             //make sure that bob get odd i.e, dp[i-j] == false (a false is an odd 
+                    break;                    //number accoding to our solution) and alice wins
+                }                             //if dp[i] is not set to true, indicates that it is an odd number and 
+            }                                 //alice looses
         }
-        return false;
+        return dp[n];
     }
 }
