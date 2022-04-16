74e86ebca1d96f08a52adf7d761248bdc9abe90b
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-04-16 17:32:32 +0530

    Time: 2 ms (65.49%), Space: 43 MB (16.94%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,20 +1,16 @@
 class Solution {
-    public int helper(String s, int index, int[] dp){
-        if(index >= s.length()) return 1;
-        if(s.charAt(index) == '0') return 0;
-        if(dp[index] != -1)
-            return dp[index];
-        int count = 0 ;
-        count += helper(s, index+1, dp);
-        if(index+1 < s.length() && (s.charAt(index)-'0')*10 + (s.charAt(index+1)-'0') < 27)
-            count += helper(s, index+2, dp);
-        dp[index] = count;
-        return dp[index];
-    }
     public int numDecodings(String s) {
-        int[] dp = new int[s.length()];
-        for(int i=0 ; i<dp.length ; i++)
-            dp[i] = -1;
-        return helper(s, 0, dp);
+        int[] dp = new int[s.length()+1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        for(int i=2 ; i<=s.length() ; i++){
+            int oneDigit = Integer.valueOf(s.substring(i-1, i));
+            int twoDigit = Integer.valueOf(s.substring(i-2, i));
+            if(oneDigit != 0)
+                dp[i] = dp[i-1];
+            if(twoDigit >= 10 && twoDigit <= 26)
+                dp[i] += dp[i-2];
+        }
+        return dp[s.length()];
     }
 }
