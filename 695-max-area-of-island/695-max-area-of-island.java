00fbd12b1b2f033829da64dda2aaf32e9ff8617b
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-04-17 18:20:43 +0530

    Time: 3 ms (73.32%), Space: 42.7 MB (80.02%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,25 +1,31 @@
 class Solution {
-    public int helper(int[][] grid, int i, int j, int[] directions){
-        if(i<0 || i>= grid.length || j<0 || j >= grid[0].length || grid[i][j] == 0 || grid[i][j] == -1)
-            return 0;
-            
-        int count = 1;
-        grid[i][j] = -1;
-        for(int k=0 ; k<directions.length ; k+=2){
-            count += helper(grid, i+directions[k], j+directions[k+1], directions);
+    public int helper(int[][] grid, int i, int j, int[] dir){
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{i,j});
+        int count = 0;
+        while(!queue.isEmpty()){
+            int[] curr = queue.remove();
+            grid[curr[0]][curr[1]] = 0;
+            count++;
+            for(int k=0 ; k < dir.length ; k+=2){
+                i = curr[0] + dir[k]; j = curr[1] + dir[k+1];
+                if(i>=0 && j>=0 && i<grid.length && j<grid[0].length){
+                    if(grid[i][j] == 1){
+                        queue.add(new int[]{i, j});
+                        grid[i][j] = 0;
+                    }
+                }
+            }
         }
         return count;
     }
     public int maxAreaOfIsland(int[][] grid) {
-        int max = 0;
-        int[] directions = new int[]{-1, 0, 0, 1, 1, 0, 0, -1};
+        int max = 0 ;
+        int[] dir = new int[]{-1, 0, 0, 1, 1, 0, 0, -1};
         for(int i=0 ; i<grid.length ; i++){
             for(int j=0 ; j<grid[i].length ; j++){
-                if(grid[i][j] == 1 && grid[i][j] != -1){
-                    int ans = helper(grid, i, j, directions);
-                    System.out.println(ans);
-                    max = Math.max(max, ans);
-                }
+                if(grid[i][j] == 1)
+                    max = Math.max(max, helper(grid, i, j, dir));
             }
         }
         return max;
