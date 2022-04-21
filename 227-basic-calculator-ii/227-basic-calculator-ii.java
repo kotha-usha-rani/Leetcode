6b7d6c4ba6d762c1363175058b5e374b5407423c
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-04-21 09:20:05 +0530

    Time: 258 ms (5.04%), Space: 55.6 MB (7.24%) - LeetHub

---
+++ 227-basic-calculator-ii/227-basic-calculator-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int calculate(String s) {
+        int num = 0;
+        char oper = '+';
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0 ; i<s.length() ; i++){
+            if(Character.isDigit(s.charAt(i)))
+                num = num*10 + (s.charAt(i)-'0');
+            if(!Character.isDigit(s.charAt(i)) && s.charAt(i) != ' '|| i==s.length()-1){
+                System.out.println(num);
+                if(oper == '-')
+                    stack.push(-num);
+                if(oper == '+')
+                    stack.push(num);
+                if(oper == '*')
+                    stack.push(stack.pop()*num);
+                if(oper == '/')
+                    stack.push(stack.pop()/num);
+                
+                num = 0;
+                oper = s.charAt(i);
+            }
+        }
+        num = 0;
+        while(!stack.isEmpty())
+            num += stack.pop();
+        return num;
+    }
+}
