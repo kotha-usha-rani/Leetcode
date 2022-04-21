be69fbfce5ce36c226c97dc9e4da653fb6a420a2
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-04-21 14:37:13 +0530

    Time: 14 ms (30.85%), Space: 43.3 MB (58.65%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -1,19 +1,30 @@
 class Solution {
     public String reverseWords(String s) {
-        StringBuilder sb = new StringBuilder();
-        for(int i=s.length()-1 ; i>=0 ; i--){
-            if(s.charAt(i) == ' ')
-                continue;
-            int j=i;
-            while(j>=0 && s.charAt(j) != ' '){
-                j--;
-            }
-            sb.append(s.substring(j+1, i+1));
-            sb.append(' ');
-            i=j;
+        // StringBuilder sb = new StringBuilder();
+        // for(int i=s.length()-1 ; i>=0 ; i--){
+        //     if(s.charAt(i) == ' ')
+        //         continue;
+        //     int j=i;
+        //     while(j>=0 && s.charAt(j) != ' '){
+        //         j--;
+        //     }
+        //     sb.append(s.substring(j+1, i+1));
+        //     sb.append(' ');
+        //     i=j;
+        // }
+        // if(sb.charAt(sb.length()-1) == ' ')
+        //     sb.deleteCharAt(sb.length()-1);
+        // return sb.toString();
+        
+        
+        String[] strs = s.trim().split("\\s+");
+        int i=0, j=strs.length-1;
+        while(i<j){
+            String temp = strs[i];
+            strs[i] = strs[j];
+            strs[j] = temp;
+            i++;j--;
         }
-        if(sb.charAt(sb.length()-1) == ' ')
-            sb.deleteCharAt(sb.length()-1);
-        return sb.toString();
+        return String.join(" ", strs);
     }
 }
