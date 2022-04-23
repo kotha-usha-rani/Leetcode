bc9b44a75d762b668f537531d0628cbb751d9eae
Kotha Usha Rani <47881212+kotha-usha-rani@users.noreply.github.com>
2022-04-23 13:59:08 +0530

    Time: 1 ms (90.85%), Space: 39.2 MB (94.94%) - LeetHub

--- 342-power-of-four/342-power-of-four.java
+++ 342-power-of-four/342-power-of-four.java
@@ -1,12 +1,5 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n==1)
-            return true;
-        if((n&1) == 1 || (n&(n-1)) != 0) return false;
-        while(n>0){
-            if(n == 1) return true;
-            n = n>>2;
-        }
-        return false;
+        return Math.log(n)/Math.log(4) % 1 == 0;
     }
 }
